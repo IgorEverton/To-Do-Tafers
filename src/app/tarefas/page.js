33413c72d0f10c0bdaa04f664c07bcb210005546
IgorEverton <evertonalves369@gmail.com>
2023-08-28 18:51:57 -0300

    fetch

--- src/app/tarefas/page.js
+++ src/app/tarefas/page.js
@@ -1,11 +1,29 @@
 import NavBar from "@/components/NavBar";
+import TarefaData from "./tarefaData";
 
-export default function Tarefas() {
+
+async function getContas(){
+  const url = "http://localhost:8080/api/tarefas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  if (!resp.ok) throw new Error("Erro Dados não carregado")
+  return resp.json()
+}
+
+export default async function Tarefas() {
+  const data = await getContas()
   return (
     <>
       <NavBar active={"tarefas"}/>
       <main className="bg-green-500 m-20 p-12 rounded-x1">
         <h2>Tarefas</h2>
+
+        <div id="data" className="text-slate-300 m-1">
+          <div id="data-row" className="flex justify-between hover:bg-slate-800 p-2 rounded">
+            <div className="flex gap-1">
+            {data.map(tarefa => <TarefaData tarefa={tarefa} /> )}
+            </div>
+          </div>
+        </div>
       </main>
     </>
 
