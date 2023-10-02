de4dcba3556a795055540885db9bc79102688ef6
IgorEverton <evertonalves369@gmail.com>
2023-10-02 20:01:27 -0300

    fix icon

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from 'react';
-import {ClipboardDocumentListIcon} from '@heroicons/react/2/outline';
+import ClipboardDocumentListIcon from '@heroicons/react/2/outline';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
