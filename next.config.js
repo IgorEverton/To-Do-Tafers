572fb429ac68f2a5946a9d5e4c2af66a6398a4d0
IgorEverton <evertonalves369@gmail.com>
2023-10-02 21:11:36 -0300

    att

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Outras configurações podem estar presentes aqui
-};
+const nextConfig = {}
+
+module.exports = nextConfig
 
 module.exports = {
-  ...nextConfig,
-  serverActions: true,
-  swcMinify: false,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(js|mjs|jsx|ts|tsx)$/,
-      use: options.defaultLoaders.babel,
-      exclude: /node_modules\/(?!@swc\/(?!.*node_modules\/))/,
-    });
-    return config;
-  },
-};
+    experimental: {
+      serverActions: true,
+    },
+  }
