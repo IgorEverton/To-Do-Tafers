742c1a7af2effea357edec3198ae9de86be5390c
IgorEverton <evertonalves369@gmail.com>
2023-10-02 21:06:13 -0300

    fix 2.0

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
-  serverActions: true,
   // Outras configurações podem estar presentes aqui
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  serverActions: true,
+  swcMinify: false,
+};
