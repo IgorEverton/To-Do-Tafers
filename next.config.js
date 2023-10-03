796270b3f38a6df869b5ffd2ee78f46e072a7ba2
IgorEverton <evertonalves369@gmail.com>
2023-10-02 21:09:12 -0300

    tentando de novo

--- next.config.js
+++ next.config.js
@@ -7,4 +7,12 @@ module.exports = {
   ...nextConfig,
   serverActions: true,
   swcMinify: false,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      use: options.defaultLoaders.babel,
+      exclude: /node_modules\/(?!@swc\/(?!.*node_modules\/))/,
+    });
+    return config;
+  },
 };
